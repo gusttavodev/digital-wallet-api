3a9999a3318833bcbe5ed117c8968c7f5dacd87b
gusttavodev <gusttavo212@gmail.com>
2022-11-16 13:42:47 -0300

    WIP

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,12 +4,18 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
         User::factory(10)->create();
+        User::create([
+            'name'     => "Admin Silva",
+            'email'    => "admin@gmail.com",
+            'password' => Hash::make('12345678'),
+        ]);
         $this->call(WalletSeeder::class);
         $this->call(TransactionSeeder::class);
     }
